e86cdf586948913eda5a2dba37ab469c2b8e9d10
JackClarkeUottawa <48411623+JackClarkeUottawa@users.noreply.github.com>
2020-02-23 12:53:25 -0500

    Update PreztoUI.py

--- PreztoUI.py
+++ PreztoUI.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'PreztoUI.ui'
-#
-# Created by: PyQt5 UI code generator 5.13.2
-#
-# WARNING! All changes made in this file will be lost!
-
 
 from PyQt5 import QtCore, QtWidgets
 import Logo_rc
